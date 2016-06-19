656bb7ec99175ced1b0a85fd63b8795fe15121a0
zach.jung <zach.jung@kakaocorp.com>
2016-06-19 13:36:38 +0900

    v1.0.2 index.pug 수정

--- app/js/index.js
+++ app/js/index.js
@@ -3,8 +3,30 @@
  */
 'use strict';
 console.log('Hello World!?');
-window.onload = function(){
+$(document).ready(function(){
 	var msg_visual = ['Gulpfile을 이용해서', '편하게 개발하자', '템플릿 엔진을 써보자', '써보면 재밌다.'];
-	
-};
+
+
+	var desc = $('.desc_article');
+	var index = 0;
+	var step = 1500;
+
+	function changeText(){
+		desc.addClass('off');
+		if(index == msg_visual.length -1){
+			index = 0;
+		}
+		index++;
+
+		setTimeout(function(){
+			desc.text(msg_visual[index]);
+			desc.removeClass('off');
+			setTimeout(changeText, step);
+		}, step);
+	}
+
+	setTimeout(changeText,step);
+});
+
+
 
